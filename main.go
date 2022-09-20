5b1da93ec4b646f7d5c89f34da9bd207a0c4c09a
Paul Johannes Wagner <hydrofin@gmail.com>
2022-09-20 14:11:56 +0200

    add first body decoding

--- main.go
+++ main.go
@@ -41,3 +41,88 @@ func decodeHeader(file *os.File) QuiteOkHeader {
 	}
 	return header
 }
+
+type Pixel struct {
+	R byte
+	G byte
+	B byte
+	A byte
+}
+
+const QOI_OP_RGB = byte(0b11111110)
+const QOI_OP_RGBA = byte(0b11111111)
+const QOI_OP_INDEX = byte(0b00000000)
+const QOI_OP_DIFF = byte(0b01000000)
+const QOI_OP_LUMA = byte(0b10000000)
+const QOI_OP_RUN = byte(0b11000000)
+
+func decodeBody(header QuiteOkHeader, file *os.File) QuiteOkFile {
+	var data []byte
+	// read raw bytes
+	{
+		dat, err := os.ReadFile("./tmp/dice.qoi")
+		if err != nil {
+			log.Fatal("read file error:", err)
+		}
+		data = dat[14:]
+	}
+	var pixels []Pixel
+
+	lastPixel := Pixel{0, 0, 0, 255}
+	seen := [64]Pixel{}
+	index := 0
+
+	for index < len(data) {
+		tag8 := data[index] & 0b11111111 // 8bit tag
+		tag2 := data[index] & 0b11000000 // 2bit tag
+
+		// gen pixel
+		var pixel Pixel
+
+		if tag8 == QOI_OP_RGB {
+			pixel = Pixel{
+				R: data[index+1],
+				G: data[index+2],
+				B: data[index+3],
+				A: 255, // TODO is this right?
+			}
+			index += 4
+		} else if tag8 == QOI_OP_RGBA {
+			pixel = Pixel{
+				R: data[index+1],
+				G: data[index+2],
+				B: data[index+3],
+				A: data[index+4],
+			}
+			index += 5
+		} else if tag2 == QOI_OP_INDEX {
+			pixelIndex := data[index+1] & 0b00111111
+			pixel = seen[pixelIndex]
+			index += 1
+		} else if tag2 == QOI_OP_DIFF {
+			dr := uint((data[index] & 0b00110000) >> 4)
+			dg := uint((data[index] & 0b00001100) >> 2)
+			db := uint((data[index] & 0b00000011) >> 0)
+			// TODO
+			index += 1
+		} else if tag2 == QOI_OP_LUMA {
+			// TODO
+			index += 2
+		} else if tag2 == QOI_OP_RUN {
+			// TODO
+			index += 1
+		}
+
+		// finish loop
+		pixels = append(pixels, pixel)
+		seen[genIndex(pixel)] = pixel
+		lastPixel = pixel
+	}
+
+	// TODO
+	return QuiteOkFile{}
+}
+
+func genIndex(pixel Pixel) uint8 {
+	return (pixel.R*3 + pixel.G*5 + pixel.B*7 + pixel.A*11) % 64
+}
